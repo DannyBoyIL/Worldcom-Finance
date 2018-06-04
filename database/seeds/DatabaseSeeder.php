b28bd7a1a1bb7c5a8dd8b7d3a9e66dd7c3e075a9
DannyBoyIL <d.r.lotem@gmail.com>
2018-06-04 11:46:55 +0300

    exeption

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,18 +1,48 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+const BAR = <<<FOOBAR
+
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
++                                      +
++     Duplicate PRIMARY KEY entry!     +
++                                      +
++     desc: DB:`Countries` table at    +
++    `abb` column have double value    +
++                                      +
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
++                                      +
++      RUN: php artisan db:seed        +
++                                      +
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+FOOBAR;
 
 class DatabaseSeeder extends Seeder {
+
 
     /**
      * Seed the application's database.
      *
      * @return void
+     * @throws Exception
      */
     public function run() {
-        $this->call(CountriesTableSeeder::class);
-        $this->call(PlacesTableSeeder::class);
-        $this->call(ZipsTableSeeder::class);
+        DB::beginTransaction();
+
+        try {
+
+            $this->call(CountriesTableSeeder::class);
+            $this->call(PlacesTableSeeder::class);
+            $this->call(ZipsTableSeeder::class);
+            DB::commit();
+
+            // all good
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw new Exception(BAR);
+        }
     }
 
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,2 @@
+<h1>Page not found, please try again</h1>
+<a href="{{ url('') }}">Go Back</a>
